b5abb94cfb2eb0567489bb4269e9d6f2926c6151
Ruslan Kostiuk <rsln.kostuk@gmail.com>
2023-09-27 10:51:06 +0300

    Init project

---
+++ src/test/java/com/rkostiuk/clearsolutionstesttask/ClearSolutionsTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkostiuk.clearsolutionstesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
